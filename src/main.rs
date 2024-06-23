97af877e21f589600d57cd43658b5c2718c4bccc
Dipesh1jain <dipesh18jain>
2024-06-23 13:41:51 +0530

    arrays

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
-fn main() {
-    let a : i32 = 10 ;
-    let b : i32 = 20 ;
-let c : i32 = &a+ &b;
+use std::io;
 
-    println!("a + b = {}", c);
+fn main() {
+let arr : [i32; 10 ]= [3,5,67,78,64,2,47,9,8,0];
+let mut getindex = String::new();
+io::stdin()
+    .read_line(&mut getindex)
+    .expect("Failed to read line ");
+let getindex: usize=  getindex.trim().parse().expect("index entered is not an number ");
+if getindex >= arr.len(){
+    println!("please enter the number within the range ");
+
+}
+else{
+    println!("element present at the index {} is : {}",getindex,arr[getindex] )
+}
 }
