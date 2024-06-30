290c1e1a4fff57ac9bfb13ef9b32f9e57c4a91a2
Dipesh1jain <dipesh18jain>
2024-06-30 19:26:22 +0530

    enums and pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,21 @@
 
 
 fn main() {
-let dimensions = Rectangle {
-    width: 30 ,
-    height : 50,
+let dipesh = Variants::A;
+let kala = Variants:: B;
+println!("{:?}",dipesh);
+println!("{:?}",kala);
+let result = match kala {
+    Variants::A => "dipesh",
+    Variants::B => "kala",
 };
-let rect1  = Rectangle {
-    width: 10,
-    height: 30 
-};
-let rect2= Rectangle {
-    height: 50,
-    width: 80
-};
-println!("can default rectangle hold rect1 {} ", dimensions.can_hold(&rect1));
-println!("can default rectangle hold rect2 {}", dimensions.can_hold(&rect2));
-// println!("square {}", dimensions::square(10));
-println!("area of a rectanlge is {dimensions:#?}" );
-println!("area of a rectanlge is {}", calculate_area(&dimensions));
-println!("area of a rectanlge through method is {}", dimensions.area());
-
+if let Variants::A = dipesh{
+    println!("hello");
 }
-fn calculate_area (rectangle : &Rectangle)-> i32 {
-     dbg! (rectangle.width * rectangle.height)
+println!("{:?}",result);
+
 }
 #[derive(Debug)]
-struct Rectangle {
-    width : i32 ,
-    height : i32
-}
-
-impl Rectangle {
-
-
-    // fn square (size : i32)-> Self {
-    //     Self {
-    //         width: size,
-    //         height: size ,
-    //     }
-    // }
-    fn area(&self) -> i32{
-        self.width * self.height
-    }
-
-    fn can_hold(&self, other: &Rectangle)->bool{
-        self.width >= other.width && self.height>= other.height
-    }
+enum Variants {
+    A, B
 }
