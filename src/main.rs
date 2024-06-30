f7be4506227940f3c9cda9525eab69981baa8555
Dipesh1jain <dipesh18jain>
2024-06-30 13:18:21 +0530

    structs example program

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,19 @@
 
 
 fn main() {
-let mut  user1_data = User {
-    name : String::from("Dipesh"),
-    sur_name : String::from("kala"),
-    age :23,
-    active: true
+let dimensions = Rectangle {
+    width: 30 ,
+    height : 50,
 };
-user1_data.sur_name = String::from("jain");
-println!("{}",user1_data.name);
-println!("{}",user1_data.sur_name);
-println!("{}",user1_data.age);
-println!("{}",user1_data.active);
+println!("area of a rectanlge is {dimensions:#?}" );
+println!("area of a rectanlge is {}", calculate_area(&dimensions));
 
-let user2_data = User {
-    sur_name: String::from("kala"),
-    ..user1_data
-};
-println!("user 2 : {}",user2_data.name);
-println!("user 2 : {}",user2_data.sur_name);
-println!("user 2 : {}",user2_data.age);
-println!("user 2 : {}",user2_data.active);
-
-let color_sruct = Color(1,0);
-println!("{}",color_sruct.0);
-println!("{}",color_sruct.1);
 }
-struct  User {
-    name : String,
-    sur_name: String,
-    age : i32,
-    active : bool 
-} 
-// tuple struct 
-struct Color(i32,i32);
+fn calculate_area (rectangle : &Rectangle)-> i32 {
+     dbg! (rectangle.width * rectangle.height)
+}
+#[derive(Debug)]
+struct Rectangle {
+    width : i32 ,
+    height : i32
+}
